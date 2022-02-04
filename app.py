e2f1e151aa107c9f36ff0e6f90681d39595ab837
amanmehra262001 <amanmehra262001@gmail.com>
2022-02-04 13:44:51 +0530

    Third commit with signin page semi completed

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
-from flask import render_template
+import email
+from pyexpat import model
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    uname = db.Column(db.String(20), primary_key=True)
+    mail = db.Column(db.String(100), nullable=False)
+    paswrd = db.Column(db.String(100), nullable=False)
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.uname} - {self.email}"
+
 
 Pokemons = ["Pikachu", "Charizard", "Squirtle", "Jigglypuff",
             "Bulbasaur", "Gengar", "Charmander", "Mew", "Lugia", "Gyarados"]
@@ -17,9 +36,19 @@ def about(name=None):
     return render_template('about.html', name=name)
 
 
-@app.route("/signin")
-def signin(name=None):
-    return render_template('signin.html', name=name)
+@app.route("/signin", methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        uname = (request.form['uname'])
+        mail = (request.form['mail'])
+        paswrd = (request.form['paswrd'])
+        user = User(uname=uname, mail=mail, paswrd=paswrd)
+        db.session.add(user)
+        # db.session.delete(model object)
+        db.session.commit()
+    createdUser = User.query.all()
+    return render_template('signin.html')
 
 
-app.run(use_reloader=True, debug=True)
+if __name__ == "__main__":
+    app.run()
