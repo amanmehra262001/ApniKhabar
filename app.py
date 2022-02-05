20381e19d98caf0687bed59a860dd5a2c972cc33
amanmehra262001 <amanmehra262001@gmail.com>
2022-02-05 21:10:43 +0530

    Signin functionality completed along with prefrences options

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from sqlalchemy import null
+
 app = Flask(__name__)
 app.secret_key = "super secret"
 
@@ -12,11 +14,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+uname = null
+paswrd = null
+mail = null
+userprefrencesstr = null
+
 
 class User(db.Model):
     uname = db.Column(db.String(20), primary_key=True)
     mail = db.Column(db.String(100), nullable=False)
     paswrd = db.Column(db.String(100), nullable=False)
+    userprefrencesstr = db.Column(db.String(200))
     date_time = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
@@ -49,6 +57,7 @@ def signin():
 
 @app.route("/signin", methods=['POST'])
 def checksignin():
+    global uname, mail, paswrd
     uname = request.form['uname']
     pss = request.form['paswrd']
     print(uname, pss)
@@ -62,12 +71,11 @@ def checksignin():
 
     else:
         if request.method == 'POST':
+
             uname = (request.form['uname'])
             mail = (request.form['mail'])
             paswrd = (request.form['paswrd'])
-            user = User(uname=uname, mail=mail, paswrd=paswrd)
-            db.session.add(user)
-            db.session.commit()
+
             # occupied = "userregistered"
             return redirect('/pref')
 
@@ -94,9 +102,20 @@ def pref():
             except:
                 userunprefered.append(topics)
         print(userprefrences)
+        if len(userprefrences) > 0:  # if list not empty
+            # converting python list to string
+            userprefrencesstr = ' '.join(userprefrences)
+        else:  # if list is empty
+            userprefrencesstr = "general business sports movies education lifestyle art"
+        user = User(uname=uname, mail=mail, paswrd=paswrd,
+                    userprefrencesstr=userprefrencesstr)
+        db.session.add(user)
+        db.session.commit()
+        # print(userprefrencesstr)
         return redirect('/signin')
     return render_template('pref.html')
 
 
+# business movies shopping crypto inovations
 if __name__ == "__main__":
     app.run(debug=True)
